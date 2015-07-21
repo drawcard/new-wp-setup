d1aa9e58ac11f28840e1c37caaa192b06962b58d
Chris @ Drawcard <signup@drawcard.com.au>
2015-07-21 15:47:59 +0930

    Added regenerate thumbnails plugin

--- new-wp-setup.sh
+++ new-wp-setup.sh
@@ -148,6 +148,7 @@ if [[ $answer = y ]] ; then
   wp plugin install --activate tinymce-advanced # https://wordpress.org/plugins/tinymce-advanced/
   wp plugin install --activate wordpress-seo # https://wordpress.org/plugins/wordpress-seo/
   wp plugin install --activate wp-emmet # https://wordpress.org/plugins/wp-emmet/
+  wp plugin install --activate regenerate-thumbnails # https://wordpress.org/plugins/regenerate-thumbnails/
   ## WP sync plugins
   wp plugin install --activate https://github.com/wp-sync-db/wp-sync-db/archive/master.zip # https://github.com/wp-sync-db/wp-sync-db
   wp plugin install --activate https://github.com/wp-sync-db/wp-sync-db-media-files/archive/master.zip # https://github.com/wp-sync-db/wp-sync-db-media-files
